884348666c02f0a89dbf141702cb16813df81d91
Tehzeeb07 <tehzeeb.shkk@gmail.com>
2026-08-07 17:25:35 +0300

    ui

--- components/admin/Sidebar.tsx
+++ components/admin/Sidebar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { LogoutButton } from "@/components/shared/LogoutButton";
 import { usePathname } from "next/navigation";
 
 const navItems = [
