2f4d6297214062e689ad6e45014a6dce8858777a
Tehzeeb07 <tehzeeb.shkk@gmail.com>
2026-08-07 17:23:11 +0300

    login update

--- components/shared/LogoutButton.tsx
+++ components/shared/LogoutButton.tsx
@@ -1,1 +1,15 @@
-;
+"use client";
+
+import { useAuthActions } from "@convex-dev/auth/react";
+
+export function LogoutButton() {
+  const { signOut } = useAuthActions();
+  return (
+    <button
+      onClick={() => signOut()}
+      className="text-sm text-gray-500 hover:text-gray-800"
+    >
+      Log out
+    </button>
+  );
+}
